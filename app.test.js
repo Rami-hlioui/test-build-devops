ffef43791cf1f202df2a30f4a2eb346ca28e583a
Rami-hlioui <ramihlioui.tsuna@gmail.com>
2022-03-12 16:22:08 +0100

    6565

--- app.test.js
+++ app.test.js
@@ -8,7 +8,7 @@ describe('Test Handlers', function () {
         request(app)
         .get('/')
         .expect(200)
-        .expect('Hello, World : our first test!', done)
+        .expect('He', done)
       
     });
 });
